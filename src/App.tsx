be9ae9e7e24481281c2d786af6234eea3e61c4a6
millan-figueroa <millan.fig@gmail.com>
2025-11-14 21:20:44 -0800

    Imports usePagination hook into parent component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { usePagination } from "./hooks/usePagination";
+
 function App() {
-  return <></>;
+  // assuming we got 105 pages total
+  const totalItems = 105;
+
+  // call custom hook
+  const { currentPage, totalPages, canNextPage, canPrevPage } = usePagination(
+    totalItems,
+    10, // items per page
+    1 // page to start on
+  );
+
+  return (
+    <div>
+      <h1>Pagination Demo</h1>
+
+      <p>Current Page: {currentPage}</p>
+      <p>Total Pages: {totalPages}</p>
+
+      <p>Can go to next page: {canNextPage ? "yes" : "no"}</p>
+      <p>Can go to previous page: {canPrevPage ? "yes" : "no"}</p>
+    </div>
+  );
 }
 
 export default App;
