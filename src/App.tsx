7e4e91f7b380dc299fc214500e92b5893dc3e1c5
millan-figueroa <millan.fig@gmail.com>
2025-11-14 22:13:26 -0800

    Creates fake items and show current page items, adds some styles to parent component

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,34 @@ function App() {
     1 // page to start on
   );
 
+  // create fake items
+  const items = [];
+  for (let i = 0; i < totalItems; i++) {
+    items.push("Item " + (i + 1));
+  }
+
+  // figure out which items to show on this page
+  const startIndex = (currentPage - 1) * 10;
+  const endIndex = Math.min(startIndex + 9, totalItems - 1);
+  // creates a new array containing only the items for the curren
+  const visibleItems = items.slice(startIndex, endIndex + 1);
+
   return (
-    <div>
-      <h1>Pagination Demo</h1>
+    <div className="p-6 max-w-2xl mx-auto text-gray-200 bg-black rounded-lg">
+      <h2 className="text-xl font-semibold text-center mb-6">
+        Pagination Demo
+      </h2>
+
+      <ul className="space-y-1 mb-6">
+        {visibleItems.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
       <p>Current Page: {currentPage}</p>
       <p>Total Pages: {totalPages}</p>
       <p>Can go to next page: {canNextPage ? "yes" : "no"}</p>
       <p>Can go to previous page: {canPrevPage ? "yes" : "no"}</p>
-      // test next and previous page
+      {/* test next and previous page */}
       <button onClick={prevPage}>Prev</button>
       <button onClick={nextPage}>Next</button>
       <button onClick={() => setPage(2)}>Go to page 2</button>
